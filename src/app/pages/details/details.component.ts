bcbe3962df76193666ed498667b50fd8443f1b16
Davi <davisilvaqueiroz@outlook.com>
2023-05-25 01:56:35 -0300

    foi feito um fork join para juntar as requisições da details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'http://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'http://pokeapi.co/api/v2/pokemon-species';
 
+  constructor(private route: ActivatedRoute, private pokeApi: PokeApiService){}
+
+
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+
+  get pokemon(){
+    const id = this.route.snapshot.params['id'];
+    const pokemon = this.pokeApi.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApi.apiGetPokemons(`${this.urlName}/${id}`);
+
+    forkJoin([pokemon, name]).subscribe({
+      next: (res: any) => console.log(res),
+      error: (error: any) => error
+    });
+    return
+  }
 }
